b14ace24eca94de1d16b87dc7a73b073ade1d4f4
Tim Schaefer <ts+code@rcmd.org>
2018-11-01 15:27:29 +0100

    Make important functions available directly from package.

---
+++ src/brainview/__init__.py
@@ -0,0 +1,8 @@
+"""
+Brainview high-level API functions.
+"""
+
+# The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
+__all__ = [ 'get_brain_view' ]
+
+from .singleview import get_brain_view
