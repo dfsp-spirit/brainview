db6f4e251e6b9f3f8a694581ed4197242e35f58d
Tim Schaefer <ts+code@rcmd.org>
2018-11-03 15:33:00 +0100

    add viewer app, parse args, display

--- bin/brainviewer.py
+++ bin/brainviewer.py
@@ -6,18 +6,18 @@ import mayavi.mlab as mlab
 import brainview as bv
 import argparse
 
-# To run this in dev mode (in virtual env, pip -e install of brainview active):
-# cd REPO_ROOT
-# PYTHONPATH=./src/brainview python bin/brainviewer.py --help
+# To run this in dev mode (in virtual env, pip -e install of brainview active) from REPO_ROOT:
+# PYTHONPATH=./src/brainview python bin/brainviewer.py tim -d ~/data/tim_only/
 
 def brainviewer():
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="View brains.")
-    parser.add_argument("subject", help="The subject you want to load. Directory name in the subjects_dir.")
-    parser.add_argument("-d", "--subjects_dir", help="The subjects_dir containing the subject.", default="")
-    parser.add_argument("-m", "--measure", help="The measure to load.", default="area")
-    parser.add_argument("-s", "--surface", help="The surface to load.", default="white")
+    parser.add_argument("subject", help="The subject you want to load. String, a directory under the subjects_dir.")
+    parser.add_argument("-d", "--subjects_dir", help="The subjects_dir containing the subject. Defaults to environment variable SUBJECTS_DIR.", default="")
+    parser.add_argument("-m", "--measure", help="The measure to load. String, defaults to 'area'. ", default="area")
+    parser.add_argument("-s", "--surface", help="The surface to load. String, defaults to 'white'.", default="white")
+    parser.add_argument("-e", "--hemi", help="The hemisphere to load. One of ('both', 'lh, 'rh'). Defaults to 'both'.", default="both")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity.", action="store_true")
     args = parser.parse_args()
 
@@ -32,11 +32,15 @@ def brainviewer():
         subjects_dir = args.subjects_dir
 
     subject_id = args.subject
+    measure = args.measure
+    surface = args.surface
+    hemi = args.hemi
 
-    vert_coords, faces, morphometry_data, meta_data = bl.subject(subject_id, subjects_dir=subjects_dir)
+    vert_coords, faces, morphometry_data, meta_data = bl.subject(subject_id, subjects_dir=subjects_dir, measure=measure, surf=surface, hemi=hemi)
 
-    fig = mlab.figure(1, bgcolor=(1, 1, 1), size=(800, 600))
-    brain_mesh = sv.get_brain_view(vert_coords, faces, morphometry_data)
+    fig_title = 'Brainviewer: %s: %s of surface %s' % (subject_id, measure, surface)
+    fig = mlab.figure(fig_title, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(800, 600))
+    brain_mesh = bv.get_brain_view(vert_coords, faces, morphometry_data)
     bv.show()
 
 
--- src/brainview/__init__.py
+++ src/brainview/__init__.py
@@ -3,6 +3,6 @@ Brainview high-level API functions.
 """
 
 # The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
-__all__ = [ 'get_brain_view' ]
+__all__ = [ 'get_brain_view', 'show' ]
 
-from .singleview import get_brain_view
+from .singleview import get_brain_view, show
