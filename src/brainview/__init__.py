1bd384b8d06ee8c3076faf3710c07e978e0bde68
Tim Schaefer <ts+code@rcmd.org>
2018-11-07 22:38:30 +0100

    set version in code

--- src/brainview/__init__.py
+++ src/brainview/__init__.py
@@ -5,4 +5,6 @@ Brainview high-level API functions.
 # The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
 __all__ = [ 'get_brain_view', 'show' ]
 
+__version__ = '0.0.1dev'
+
 from .singleview import get_brain_view, show
