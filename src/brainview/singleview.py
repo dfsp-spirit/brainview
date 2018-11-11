3770fb0666641e4f1f1e9ae03009d360aabbc465
Tim Schaefer <ts+code@rcmd.org>
2018-11-11 16:09:13 +0100

    Add some documentation, use triangular_mesh_source instead of triangular_mesh to fix single hemi issue.

--- src/brainview/singleview.py
+++ src/brainview/singleview.py
@@ -7,11 +7,15 @@ These functions provide a single view.
 import numpy as np
 import brainload as bl
 import brainload.spatial as st
-import warnings
 import mayavi.mlab as mlab
 
+
 def _print_mlab_view():
-    '''See http://docs.enthought.com/mayavi/mayavi/auto/mlab_camera.html#view for details.'''
+    """
+    Print the camera properties in the current mlab view.
+
+    See http://docs.enthought.com/mayavi/mayavi/auto/mlab_camera.html#view for details.
+    """
     view = mlab.view()
     print "[View] azimuth=%f, elevation=%f, distance=%f" % (view[0], view[1], view[2])
 
@@ -33,21 +37,56 @@ def _get_mayavi_mesh(vert_coords, faces, **kwargs):
     return mayavi_mesh
 
 
+def _get_mayavi_source_mesh(vert_coords, faces, morphometry_data, **kwargs):
+    x, y, z = st.coords_a2s(vert_coords)
+    src_mesh = mlab.pipeline.triangular_mesh_source(x, y, z, faces)
+    src_mesh.data.points = vert_coords
+    #src_mesh.data.point_data.normals = geo.nn
+    src_mesh.data.cell_data.normals = None
+    surf = mlab.pipeline.surface(src_mesh, reset_zoom=True)
+    surf.actor.property.backface_culling = True
+
+
 def get_brain_view(vert_coords, faces, morphometry_data, **kwargs):
     """
-    Convenience wrapper around _get_mayavi_mesh. Creates a mayavi mesh from the vert_coords, faces and morphometry_data.
+    Create a mayavi mesh from data.
+
+    Create a mayavi mesh from the vert_coords, faces and morphometry_data. Additional keyword arguments will be passed on to the call to the mlab.triangular_mesh function.
+
+    Parameters
+    ----------
+    vert_coords: 2D numpy array of shape (n_verts, 3)
+        An array of vertex corrdinates. Each vertex position is identified by an x, y, and z coordinate.
+
+    faces: 2D numpy array of shape (n_faces, 3)
+        An array of 3-faces, i.e., each face has to consists of 3 vertices. The 3 vertices are indices into the vert_coords array.
+
+    morphometry_data: 1D numpy array of shape (n_verts, )
+        Assigns a scalar value to each vertex.
+
+    kwargs: extra keyword arguments
+        Will be passed on to the call to the mayavi mesh function.
 
     Returns
     -------
     surface: mayavi.modules.surface.Surface
+        The resulting surface. It gets added to the current scene by default and potentially triggers actions in there (like camera re-orientation), use kwargs to change that behaviour.
     """
     print "min=%f max=%f" % (np.min(morphometry_data), np.max(morphometry_data))
-    return _get_mayavi_mesh(vert_coords, faces, scalars=morphometry_data, **kwargs)
+    #return _get_mayavi_mesh(vert_coords, faces, scalars=morphometry_data, **kwargs)
+    return _get_mayavi_source_mesh(vert_coords, faces, morphometry_data, **kwargs)
 
 
 def activate_overlay(meta_data):
     """
-    Displays various information from the given meta_data dictionary in the view of the currently active mlab figure.
+    Display a very simple meta_data overlay in the current view.
+
+    Display various information from the given meta_data dictionary in the view of the currently active mlab figure.
+
+    Parameters
+    ----------
+    meta_data: dictionary
+        A dictionary with meta_data that has been created manually or received from a braibload function when loading the subject data. The entries 'subject_id', 'surf', and 'measure' or accessed to create the overlay text.
     """
     mlab.colorbar()
     mlab.title('Brain of subject ' + meta_data.get('subject_id', '?'), size=0.4)
@@ -57,7 +96,24 @@ def activate_overlay(meta_data):
 
 def export_figure(fig_handle, export_file_name_with_extension, silent=False, **kwargs):
     """
-    Exports the figure identified by the given fig_handle to the file. File must be valid path to a writable location, and the file extension must be one that is supported by mlab.savefig.
+    Export the view of the scene to an image file.
+
+    Export the figure identified by the given fig_handle to the file. File must be valid path to a writable location, and the file extension must be one that is supported by `mlab.savefig`.
+
+    Parameters
+    ----------
+    fig_handle: mlab figure handle
+        Handle for the figure
+
+    export_file_name_with_extension: string
+        Path to a file, the extension determines the output format. Example: '/tmp/brain.png'
+
+    silent: bool, optional
+        Whether to be silent, i.e., NOT print a message to stdout that informs the user of the location where the file was saved. Defaults to False.
+
+    **kwargs: any keyword arguments, optional
+        Will be passed on to the call to `mlab.savefig`.
+
     """
     if not silent:
         print "Exporting scene to file '%s'." % export_file_name_with_extension
@@ -66,13 +122,19 @@ def export_figure(fig_handle, export_file_name_with_extension, silent=False, **k
 
 def show():
     """
-    Renders and displays the currently active mayavi scene in an interactive window.
-    This requires a GUI and a working setup of matplotlib with proper backend configuration on the machine.
+    Display the currently active mayavi scene in an interactive window.
+
+    Render and display the currently active mayavi scene in an interactive window. This requires a GUI and a working setup of matplotlib with proper backend configuration on the machine. Currently does nothing but to call `mlab.show()`.
     """
     mlab.show()
 
 
-def get_brain_multi_view(vert_coords, faces, morphometry_data):
+def _get_brain_multi_view(vert_coords, faces, morphometry_data):
+    """
+    Experimental, ignore.
+
+    Copy the original mesh, then rotate and translate it to get another view. Fakes a very simple and stupid multi-view in a single view.
+    """
     mesh_center = get_brain_view(vert_coords, faces, morphometry_data)
     x, y, z = st.coords_a2s(vert_coords)
 
