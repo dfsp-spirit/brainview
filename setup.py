b333b64b90075e0d3626ef72fa6af2aae0b89f87
Tim Schaefer <ts+code@rcmd.org>
2018-11-21 11:22:25 +0100

    add python 2 tag

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
           'Intended Audience :: Developers',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7'],
     license='MIT',
     setup_requires=['pytest-runner'],
