b0551b0b8fa7a75b353dedd16b24976b2b744d2f
Tim Schaefer <ts+code@rcmd.org>
2018-11-25 22:37:45 +0100

    expose util function for cfg in brainview namespace

--- src/brainview/__init__.py
+++ src/brainview/__init__.py
@@ -3,9 +3,9 @@ Brainview high-level API functions.
 """
 
 # The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
-__all__ = [ 'brain_morphometry_view', 'brain_label_view', 'brain_atlas_view', 'show', 'get_config', 'get_default_config_filename' ]
+__all__ = [ 'brain_morphometry_view', 'brain_label_view', 'brain_atlas_view', 'show', 'get_config', 'get_default_config_filename', 'cfg_getboolean', 'cfg_getint', 'cfg_get', 'cfg_getfloat' ]
 
 __version__ = '0.0.1'
 
 from .singleview import brain_morphometry_view, brain_label_view, brain_atlas_view, show
-from .util import get_config, get_default_config_filename
+from .util import get_config, get_default_config_filename, cfg_getboolean, cfg_getint, cfg_get, cfg_getfloat
